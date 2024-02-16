e3397f7cb18ceb3b4ea41e62ab87f54f08872248
Useungmin <146905629+Useungmin@users.noreply.github.com>
2024-02-16 18:55:36 +0900

    3차 프로젝트 승민 02-16 18:55

--- src/main/java/com/green/hoteldog/manager/ManagerRepository.java
+++ src/main/java/com/green/hoteldog/manager/ManagerRepository.java
@@ -1,4 +1,23 @@
 package com.green.hoteldog.manager;
 
-public interface ManagerRepository {
+import com.green.hoteldog.common.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ManagerRepository extends JpaRepository<UserEntity,Long> {
+
+    //모든 유저
+    List<UserEntity> findAllByOdrerbycreatedAtdesc();
+    // 사업자 유저
+    List<UserEntity> findAllByAccountStatusOrderByCreatedAtDesc(Long accountStatus);
+    // 일반유저
+    List<UserEntity> findAllByAccountStatusNotOrderByCreatedAtDesc(Long accountStatus);
+
+    //List<UserEntity> normalUsers = managerRepository.findAllByAccountStatusNotOrderByCreatedAtDesc(1L);
+
+    //accountStatus  OdrerbycreatedAtdesc
+   // List<UserEntity> normalUsers = managerRepository.findAllByAccountStatusOrderByCreatedAtDesc(1L);
+
 }
--- src/main/java/com/green/hoteldog/manager/ManagerService.java
+++ src/main/java/com/green/hoteldog/manager/ManagerService.java
@@ -1,11 +1,19 @@
 package com.green.hoteldog.manager;
 
+import com.green.hoteldog.common.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ManagerService {
+    private final ManagerRepository managerRepository;
+
+    public List <UserEntity> getAllUser(){
+        return managerRepository.findAllByOdrerbycreatedAtdesc();
+    }
 }
