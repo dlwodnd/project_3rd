f55c2a34f94274598e69d1e858515d8b5db90ee4
Useungmin <146905629+Useungmin@users.noreply.github.com>
2024-02-26 09:50:06 +0900

    Changes

--- src/main/java/com/green/hoteldog/common/repository/HotelRepository.java
+++ src/main/java/com/green/hoteldog/common/repository/HotelRepository.java
@@ -4,10 +4,12 @@ import com.green.hoteldog.common.entity.BusinessEntity;
 import com.green.hoteldog.common.entity.HotelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
 
+    List<HotelEntity> findByBusinessEntity_AccountStatus(int accountStatus);
     //승민
     //승민
 
